10fdf1d71dd9dadf232ddc087a063d60b716ee85
Sam <samuel.fitz@btinternet.com>
2023-12-10 17:21:54 +0000

    feat: add other pages, routes set up and experiment with aurelia list rendering

--- src/my-app.ts
+++ src/my-app.ts
@@ -2,27 +2,47 @@ export class MyApp {
   static routes = [
     {
       path: "",
-      component: () => import("./components/home-page"),
+      component: () => import("./pages/home-page"),
       id: "Home",
       title: "Home",
     },
     {
-      path: "",
-      component: () => import("./components/contact-page"),
-      id: "Home",
-      title: "Home",
+      path: "/products",
+      component: () => import("./pages/products-page"),
+      id: "Products",
+      title: "Products",
     },
     {
-      path: "",
-      component: () => import("./components/home-page"),
-      id: "Home",
-      title: "Home",
+      path: "/customers",
+      component: () => import("./pages/customers-page"),
+      id: "Customers",
+      title: "Customers",
     },
     {
-      path: "",
-      component: () => import("./components/home-page"),
-      id: "Home",
-      title: "Home",
+      path: "/news",
+      component: () => import("./pages/news-page"),
+      id: "News",
+      title: "News",
+    },
+    {
+      path: "/about",
+      component: () => import("./pages/about-page"),
+      id: "About",
+      title: "About",
+    },
+    {
+      path: "/contact",
+      component: () => import("./pages/contact-page"),
+      id: "ContactPage",
+      title: "Contact Page",
     },
   ];
+
+  public links = [
+    {path: "/", name: "Home"},
+    {path: "/products", name: "Products & Services"},
+    {path: "/customers", name: "Our Customers"},
+    {path: "/news", name: "News & Events"},
+    {path: "/about", name: "About Us"},
+  ]
 }
